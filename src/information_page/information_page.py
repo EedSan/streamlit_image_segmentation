cc279d4c2de54ca5874c9fe4e6dfeaae604d3edc
Eugene Sazonenko <55549442+EedSan@users.noreply.github.com>
2023-06-13 17:56:22 +0300

    Update information_page.py

--- src/information_page/information_page.py
+++ src/information_page/information_page.py
@@ -38,7 +38,7 @@ def default_page():
         |U-Net|1.60*10^-4|0.9999|0.9408|0.8582|
         |SegNet|9.5510^-4|0.9998|0.7488|0.6338|
         |DeconvNet|0.0010|0.9996|0.6984|0.3451|       
-    """)
+    """
     expander_results.write("""
         <style>
         [data-testid="stMarkdownContainer"] tbody tr:nth-child(2n) {
